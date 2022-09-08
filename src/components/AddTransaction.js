93007106c29f8a15ee7f386159c6d6ab58a23217
Said Hussein <saidhussein12342@gmail.com>
2022-09-08 14:53:38 +0300

    Added transaction form and date field.

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import AddIcon from '@mui/icons-material/Add';
+import TextField from '@mui/material/TextField';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 function AddTransaction() {
+    const [value, setValue] = useState(null);
     return(
         <Container>
            <Typography
@@ -15,6 +20,20 @@ function AddTransaction() {
            >
             Add a Transaction
            </Typography>
+           <form noValidate autocomplete="off">
+           <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DatePicker
+                label="Transaction Date"
+                variant="outlined"
+                color="secondary"
+                value={value}
+                onChange={(newValue) => {
+                setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} />}
+                />
+            </LocalizationProvider>
+           </form>
            <Button
            type="submit"
            color="secondary"
