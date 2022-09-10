1bea17a7fc2122ecbd5ddd4630758e0b07699a43
Said Hussein <saidhussein12342@gmail.com>
2022-09-11 01:50:28 +0300

    Update transaction and patch requests.

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 import DeleteIcon from '@material-ui/icons/Delete';
 import CreateIcon from '@material-ui/icons/Create';
 import IconButton from '@material-ui/core/IconButton';
+import CheckIcon from '@material-ui/icons/Check';
 
 function Transaction({ date, description, amount, category, id, transactions, setTransactions}) {
+  const [edit, setEdit] = useState(false);
+  const [obj, setObj] = useState({
+    date: date,
+    description: description,
+    amount: amount,
+    category: category,
+  });
+
+  function handleEditTransaction() {
+    setEdit((edit) => !edit)
+  }
+
+  function handleInputUpdate(updatedData){
+    const updatedTransactions = transactions.map(transaction=>{
+      if (transaction.id === updatedData.id) {
+        return updatedData;
+      } else {
+        return transaction;
+      }
+    })
+    setTransactions(updatedTransactions);
+  }
+
+  function handleChange(e) {
+      let name = e.target.name
+      let value = e.target.value
+      setObj({
+        [name]: value
+      });
+      console.log(obj)
+  }
+
+  function handleUpdateTransaction() {
+    fetch(`http://localhost:8000/transactions/${id}`, {
+      method: "PATCH",
+      headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(obj),
+    })
+      .then((res) => res.json())
+      .then((transactions) => {
+        console.log(transactions)
+        handleInputUpdate(transactions)})
+  }
 
   function handleDeleteTransaction() {
     fetch(`http://localhost:8000/transactions/${id}`, {
@@ -19,14 +65,14 @@ function Transaction({ date, description, amount, category, id, transactions, se
       setTransactions(revisedTransactions)
     })
   }
-  
+
    return (
           <TableRow>
-            <TableCell>{date}</TableCell>
-            <TableCell align="right">{description}</TableCell>
-            <TableCell align="right">{amount}</TableCell>
-            <TableCell align="right">{category}</TableCell>
-            <TableCell align="right">{<IconButton aria-label="update"><CreateIcon /></IconButton>}</TableCell>
+            <TableCell contentEditable={edit} onChange={handleChange} name={date} value={date}>{date}</TableCell>
+            <TableCell align="right" contentEditable={edit} onChange={handleChange} name={description} value={description}>{description}</TableCell>
+            <TableCell align="right" contentEditable={edit} onChange={handleChange} name={amount} value={amount}>{amount}</TableCell>
+            <TableCell align="right" contentEditable={edit} onChange={handleChange} name={category} value={category}>{category}</TableCell>
+            <TableCell align="right" onClick={handleEditTransaction}>{edit ? <IconButton onClick={handleUpdateTransaction}><CheckIcon /></IconButton> : <IconButton><CreateIcon /></IconButton>}</TableCell>
             <TableCell align='right'>{<IconButton aria-label="delete" onClick={handleDeleteTransaction}><DeleteIcon /></IconButton>}</TableCell>
           </TableRow>
   );
